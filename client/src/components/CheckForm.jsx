ec13e35f67387e700a734da10707326c009f6c72
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-10-09 21:37:14 +0900

    feat: show checkform after click bottom

--- client/src/components/CheckForm.jsx
+++ client/src/components/CheckForm.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { checkUser } from '../apis/apis';
+import { useState } from 'react';
+
+import SettingForm from './SettingForm';
+
 export default function CheckForm() {
-  return <div>CheckForm</div>;
+  return (
+    <div>
+      <p className='text-center'>devlab</p>
+    </div>
+  );
 }
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMessage } from '../apis/apis';
 
+import CheckForm from '../components/CheckForm';
+
 export default function MainPage() {
   const { uuid } = useParams();
   const [message, setMessage] = useState('');
+
+  const [clickCnt, setClickCnt] = useState(0);
 
   useEffect(() => {
     getMessage(uuid).then((data) => {
@@ -16,17 +20,25 @@ export default function MainPage() {
     });
   }, []);
 
+  const clickBottom = () => {
+    setClickCnt((prevCnt) => prevCnt + 1);
+  };
+
   return (
     <div>
       <div className='w-screen p-8' style={{ height: '90vh' }}>
         <p className='text-3xl'>{message}</p>
       </div>
-      <div
-        className='text-sm text-center bg-gray-200'
-        style={{ height: '10vh' }}
-      >
-        devlab
-      </div>
+      {clickCnt < 10 && (
+        <div
+          className='text-sm text-center bg-gray-200'
+          style={{ height: '10vh' }}
+          onClick={clickBottom}
+        >
+          devlab
+        </div>
+      )}
+      {clickCnt >= 10 && <CheckForm />}
     </div>
   );
 }
